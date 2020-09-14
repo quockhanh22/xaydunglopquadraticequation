258ac3c66844a1f54df6fab6bc6964bd9c0659cb
quockhanh22 <anhkhanh11761@gmail.com>
2020-09-14 11:44:08 +0700

    xaydunglopquadraticequation
    xaydunglopquadraticequation

---
+++ QuadraticEquation.cs
@@ -0,0 +1,16 @@
+namespace XayDungLopQuadraticEquation
+{
+    internal class QuadraticEquation
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+}
